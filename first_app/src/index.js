91a8f193da47563bb433c30984900eb5a456b181
Christopher Guerrero <chrisgrr3@gmail.com>
2023-03-07 17:58:23 -0700

    Defined header JSX element, which uses Title component

--- first_app/src/index.js
+++ first_app/src/index.js
@@ -8,4 +8,12 @@ function Title() {
   return <h2>Getting Started with React</h2>
 };
 
-ReactDOM.render(Title(), rootElement)
+const header = (
+  <header>
+    <h1>Welcome to 30 Days of React</h1>
+    <Title />
+    <h3>JavaScript Library</h3>
+  </header>
+)
+
+ReactDOM.render(header, rootElement)
