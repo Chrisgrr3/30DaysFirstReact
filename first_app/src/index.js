d17a725da28d0051362a186437163ebd87c696a9
Christopher Guerrero <chrisgrr3@gmail.com>
2023-03-07 17:55:49 -0700

    Defined Title component and rendered to the DOM

--- first_app/src/index.js
+++ first_app/src/index.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const jsxElement = <h1>This is a JSX element</h1>
+const jsxElement = <h1>This is a JSX element.</h1>
 const rootElement = document.getElementById('root')
 
-ReactDOM.render(jsxElement, rootElement)
+function Title() {
+  return <h2>Getting Started with React</h2>
+};
+
+ReactDOM.render(Title(), rootElement)
