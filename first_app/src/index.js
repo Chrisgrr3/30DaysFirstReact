94527edbd278fa47e853ab7c35f57888b076d5c4
Christopher Guerrero <chrisgrr3@gmail.com>
2023-03-07 18:00:45 -0700

    Created tech variable, which is used in header JSX and Title component

--- first_app/src/index.js
+++ first_app/src/index.js
@@ -4,13 +4,15 @@ import ReactDOM from 'react-dom'
 const jsxElement = <h1>This is a JSX element.</h1>
 const rootElement = document.getElementById('root')
 
+const tech = 'React'
+
 function Title() {
-  return <h2>Getting Started with React</h2>
+  return <h2>Getting Started with {tech}</h2>
 };
 
 const header = (
   <header>
-    <h1>Welcome to 30 Days of React</h1>
+    <h1>Welcome to 30 Days of {tech}</h1>
     <Title />
     <h3>JavaScript Library</h3>
   </header>
